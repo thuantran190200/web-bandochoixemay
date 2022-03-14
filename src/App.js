57915aafd72fe04d069e49c5dbfd0d97a9e2b9ea
Thuan Tran <thuantran190200@gmail.com>
2022-03-14 13:58:28 +0700

    error page home

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
+import Home  from './pages/home';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const name = "Thuận Trần";
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Thuận Trần
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <div>
-      <h1>Họ tên: {name}</h1>
-    </div>
-    </div>
-  );
+  return <Home />;
 }
 
 function Menu(){
