cbd7675e8e4b955879bed7cd76a3af0ad7166065
Thuan Tran <thuantran190200@gmail.com>
2022-03-15 11:19:58 +0700

    update form

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = "Thuận Trần";
   return <Home />;
-}
-
-function Menu(){
-  const name = "Thuận Trần";
-  
-  return (
-    <div>
-      <h1>Họ tên: {name}</h1>
-    </div>
-  );
 }
 
 export default App;
